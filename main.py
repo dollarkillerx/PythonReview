ac3a1e7db0cc2a870eb54c48fecf0be3a6ce74e5
Dollarkillerx <dollarkiller@vip.qq.com>
2019-11-03 08:47:41 +0800

    ad

--- main.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*- 
-# @Time : 2019-11-02 18:07 
-# @Author : DollarKillerx
-# @Description :
-from test import func
-
-if __name__ == '__main__':
-    a,b = func.hhe(1,2)
-    print(a,b)
